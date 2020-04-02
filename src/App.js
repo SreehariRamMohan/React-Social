f2f657a41aa7dd2018bdece6802be3a096c93785
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2020-04-02 16:24:29 -0700

    Basic functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
+import profile from './res/profile.JPG'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Post from "./Post/Post"
+
+
+class App extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: "",
+      messages: []
+    }
+
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.postButton = this.postButton.bind(this);
+  }
+
+  handleTextChange(event) {
+    console.log(event.target.value);
+    this.setState({
+      message: event.target.value
+    })
+  }
+
+  postButton() {
+    this.setState({
+      messages: [this.state.message, ...this.state.messages]
+    });
+
+    this.setState({
+      message: ""
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="container">
+       
+  
+          <div className="card">
+            <p>Create post</p>
+  
+            <div className="postContainer">
+              <img src={profile} className="profilePost"></img>
+              <textarea onChange={this.handleTextChange} 
+              className="postInput"
+              value={this.state.message}
+              />
+            </div>
+  
+            <div className="postButtonContainer">
+              <button 
+              className="postButton"
+              onClick={this.postButton}
+              >Post</button>
+            </div>
+            
+  
+  
+          </div>
+
+          {
+            this.state.messages.map((message) => <Post messageContent={message}/>)
+          }
+         
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/Post/Post.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import profile from '../res/profile.JPG'
+import './Post.css'
+
+class Post extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="card">
+                <div className="postContainer">
+                    <img src={profile} className="profilePost"></img>
+                    <textarea onChange={this.handleTextChange}
+                        className="postInput"
+                        value={this.props.messageContent}
+                        readonly
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Post
